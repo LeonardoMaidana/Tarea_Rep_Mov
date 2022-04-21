87e9ccbed2c642c8ab9542d47b3de02d6ebe768f
Leonardo Maidana <leo-lomas1@hotmail.com>
2022-04-21 05:26:27 -0300

    Update_jump
    Mejore el salto para que se asemeje al juego.

--- Personaje/Personaje.gd
+++ Personaje/Personaje.gd
@@ -4,6 +4,7 @@ const moveSpeed = 95
 const maxSpeed = 120
 
 const jumpHeight = -500
+const limitJump = -800
 const up = Vector2(0,-1)
 const gravity = 15
 
@@ -15,6 +16,10 @@ func _physics_process(delta):
 	motion.y +=gravity
 	var friction=false
 	
+	if Input.is_action_just_released("ui_up") or Input.is_action_just_released("ui_left") or Input.is_action_just_released("ui_right"):
+		motion.x = 0
+		motion.y = 0
+		
 	if Input.is_action_pressed("ui_right"):
 
 	   motion.x = min(motion.x + moveSpeed, maxSpeed)
@@ -27,12 +32,14 @@ func _physics_process(delta):
 		friction = true
 		
 	if is_on_floor():
-		if Input.is_action_pressed("ui_up"):
-			motion.y = jumpHeight
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = max(motion.y + jumpHeight, limitJump)
+
 		if friction ==true:
 			motion.x = lerp(motion.x,0,0.5)
 	else:
 		if friction ==true:
 			motion.x=lerp(motion.x,0,0.01)
-		
-	motion = move_and_slide(motion,up);
+			
+	motion = move_and_slide(motion,up)
+	
